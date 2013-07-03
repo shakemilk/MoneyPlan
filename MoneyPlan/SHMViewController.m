222b343bb6e97d927f29e3dc7459b81b75f9dbee
Mikhail Grushin <grushin@denivip.ru>
2013-07-03 12:16:59 +0400

    changed project structure
    - removed MoneyPlan directory

---
+++ MoneyPlan/SHMViewController.m
@@ -0,0 +1,29 @@
+//
+//  SHMViewController.m
+//  MoneyPlan
+//
+//  Created by Mikhail Grushin on 03.07.13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import "SHMViewController.h"
+
+@interface SHMViewController ()
+
+@end
+
+@implementation SHMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
