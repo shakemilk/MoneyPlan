3e6d6f6ad6d2a6f7600679b68fc5c2ad82f0be50
Mikhail Pogosskiy <m.pogosskiy@gmail.com>
2013-07-10 00:48:15 +0400

    Table with custom section header views. Can react on tap on section header, but opening or closing section doesn't work and switched off in CalculationScreenSectionHeaderView

---
+++ MoneyPlan/CalculationScreenSectionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  CalculationViewSectionHeaderView.h
+//  MoneyPlan
+//
+//  Created by Mikhail Pogosskiy on 07.07.13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculationViewSectionHeaderView : UIView
+
+@end
---
+++ MoneyPlan/CalculationScreenSectionHeaderView.m
@@ -0,0 +1,31 @@
+//
+//  CalculationViewSectionHeaderView.m
+//  MoneyPlan
+//
+//  Created by Mikhail Pogosskiy on 07.07.13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import "CalculationViewSectionHeaderView.h"
+
+@implementation CalculationViewSectionHeaderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
