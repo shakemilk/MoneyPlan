8d84303dba2b9c5feda8e22a2a8d8549eb10df29
Mikhail Pogosskiy <m.pogosskiy@gmail.com>
2013-08-24 17:23:11 +0400

    New UIViewController for FolksList screen
    SHMFolksListViewController added. StoryBoard modified to use this controller for the middle screen in Tab Bar controller.
    Changed the name for this tab to "Участники" in storyboard.

---
+++ MoneyPlan/SHMFolksListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHMFolksListViewController.h
+//  MoneyPlan
+//
+//  Created by Mikhail Pogosskiy on 24.08.13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHMFolksListViewController : UIViewController
+
+@end
---
+++ MoneyPlan/SHMFolksListViewController.m
@@ -0,0 +1,38 @@
+//
+//  SHMFolksListViewController.m
+//  MoneyPlan
+//
+//  Created by Mikhail Pogosskiy on 24.08.13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import "SHMFolksListViewController.h"
+
+@interface SHMFolksListViewController ()
+
+@end
+
+@implementation SHMFolksListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
