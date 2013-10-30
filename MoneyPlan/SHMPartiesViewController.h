2293b6a22139f448f333caa759c75e7fafd5b32b
Mikhail Grushin <mihail.grushin@gmail.com>
2013-10-30 19:57:49 +0400

    created parties screen
    - removed SHMViewController
    - added new screen to storyboard
    - added labels

---
+++ MoneyPlan/SHMPartiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHMPartiesViewController.h
+//  MoneyPlan
+//
+//  Created by Mikhail Grushin on 30/10/13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHMPartiesViewController : UIViewController
+
+@end
---
+++ MoneyPlan/SHMPartiesViewController.m
@@ -0,0 +1,41 @@
+//
+//  SHMPartiesViewController.m
+//  MoneyPlan
+//
+//  Created by Mikhail Grushin on 30/10/13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import "SHMPartiesViewController.h"
+#import "SHMAppearance.h"
+
+@interface SHMPartiesViewController ()
+
+@end
+
+@implementation SHMPartiesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [SHMAppearance defaultBackgroundColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
