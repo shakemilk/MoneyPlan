373e43f9a7e87474e95ad63938f0df41c2afdcb9
Mikhail Grushin <mihail.grushin@gmail.com>
2013-11-01 00:34:43 +0400

    added collection view to parties screen
    [4980643]

--- MoneyPlan/SHMPartiesViewController.m
+++ MoneyPlan/SHMPartiesViewController.m
@@ -9,11 +9,16 @@
 #import "SHMPartiesViewController.h"
 #import "SHMAppearance.h"
 #import "SHMTabBarController.h"
+#import "SHMButtonCollectionViewCell.h"
+#import "SHMPartyCollectionViewCell.h"
 
-@interface SHMPartiesViewController () <UIActionSheetDelegate>
+@interface SHMPartiesViewController () <UIActionSheetDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, SHMButtonCollectionViewCellDelegate>
 
 @property (nonatomic, strong) SHMTabBarController *tabBarController;
 @property (nonatomic, strong) UINavigationController *navigationController;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@property (nonatomic) NSUInteger numberOfEvents;
 
 @end
 
@@ -34,6 +39,8 @@
 	// Do any additional setup after loading the view.
     
     self.view.backgroundColor = [SHMAppearance defaultBackgroundColor];
+    [self.collectionView registerClass:[SHMButtonCollectionViewCell class] forCellWithReuseIdentifier:@"ButtonCell"];
+    [self.collectionView registerClass:[SHMPartyCollectionViewCell class] forCellWithReuseIdentifier:@"PartyCell"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -42,11 +49,39 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - New party button action
+#pragma mark - Collection view data source
 
-- (IBAction)newPartyAction:(id)sender {
-    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Новое событие" delegate:self cancelButtonTitle:@"Отмена" destructiveButtonTitle:nil otherButtonTitles:@"Создать событие", @"Присоединиться к событию", nil];
-    [actionSheet showInView:self.view];
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row) {
+        SHMPartyCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PartyCell" forIndexPath:indexPath];
+        return cell;
+    } else {
+        SHMButtonCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ButtonCell" forIndexPath:indexPath];
+        cell.delegate = self;
+        return cell;
+    }
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.numberOfEvents + 1;
+}
+
+#pragma mark - Collection view flow layout delegate
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(95.f, 100.f);
+}
+
+#pragma mark - Button cell delegate 
+
+-(void)buttonCellWasTappedForNewEvent:(SHMButtonCollectionViewCell *)cell {
+    [self.collectionView performBatchUpdates:^{
+        self.numberOfEvents++;
+        NSIndexPath *newIndexPath = [NSIndexPath indexPathForItem:self.numberOfEvents inSection:0];
+        [self.collectionView insertItemsAtIndexPaths:@[newIndexPath]];
+    } completion:^(BOOL finished) {
+        [self.collectionView reloadData];
+    }];
 }
 
 #pragma mark - Action Sheet delegate
