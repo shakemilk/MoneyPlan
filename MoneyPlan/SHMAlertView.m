16d8c6bd83af5bc8eced97f525c64bc3d522b0bf
Mikhail Grushin <mihail.grushin@gmail.com>
2013-11-01 23:07:07 +0400

    implemented background view private class for alert view
    - creates new alert-level window for alert view
    
    [7519942]

--- MoneyPlan/SHMAlertView.m
+++ MoneyPlan/SHMAlertView.m
@@ -7,6 +7,58 @@
 //
 
 #import "SHMAlertView.h"
+@interface SHMBackgroundView : UIView
+@property (nonatomic, strong) UIWindow *previousKeyWindow;
+@property (nonatomic, strong) UIWindow *alertWindow;
+
++(instancetype)sharedBackgroundView;
+@end
+
+@implementation SHMBackgroundView
+
++(instancetype)sharedBackgroundView {
+    static dispatch_once_t onceToken;
+    __strong static SHMBackgroundView *backgroundView;
+    dispatch_once(&onceToken, ^{
+        backgroundView = [SHMBackgroundView new];
+    });
+    
+    return backgroundView;
+}
+
+- (id)init {
+    if (self = [super initWithFrame:[UIScreen mainScreen].bounds]) {
+        self.backgroundColor = [UIColor colorWithWhite:0.f alpha:.5f];
+        
+        self.previousKeyWindow = [UIApplication sharedApplication].keyWindow;
+        
+        self.alertWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        self.alertWindow.windowLevel = UIWindowLevelAlert;
+        self.alertWindow.backgroundColor = [UIColor clearColor];
+        [self.alertWindow addSubview:self];
+        [self.alertWindow makeKeyAndVisible];
+        
+        self.hidden = YES;
+    }
+    
+    return self;
+}
+
+-(void)setHidden:(BOOL)hidden {
+    [super setHidden:hidden];
+    
+    self.alertWindow.hidden = hidden;
+    
+    if (hidden) {
+        [self.alertWindow resignKeyWindow];
+        [self.previousKeyWindow makeKeyWindow];
+    } else {
+        [self.previousKeyWindow resignKeyWindow];
+        [self.alertWindow makeKeyWindow];
+    }
+}
+
+@end
 
 @implementation SHMAlertView
 
