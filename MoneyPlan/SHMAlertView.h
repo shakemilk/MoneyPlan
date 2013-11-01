d715644837322e3dd759dfe541448cd84e3d9fab
Mikhail Grushin <mihail.grushin@gmail.com>
2013-11-01 23:05:59 +0400

    created custom alert view class
    [7519942]

---
+++ MoneyPlan/SHMAlertView.h
@@ -0,0 +1,13 @@
+//
+//  SHMAlertView.h
+//  MoneyPlan
+//
+//  Created by Mikhail Grushin on 01/11/13.
+//  Copyright (c) 2013 Shakemilk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHMAlertView : UIView
+
+@end
